a8a90ac7e836be1faa2ab0639fa2b2f9c17c97d7
Adam Rensel <adamrensel@gmail.com>
2016-04-19 10:54:48 -0400

    Add sample animation code

--- sample.js
+++ sample.js
@@ -22,10 +22,28 @@ arrow1 = new d3Arrow.Arrow()
 arrow2 = new d3Arrow.Arrow({color: "red"})
 // arrow3 = new d3Arrow.arrow({color: "blue"})
 
-arrow1.drawFromTo(".callout1", '.col2 h2')
-arrow2.drawFromTo(".callout1", '.col1 h2')
-// arrow3.drawFromTo(".callout1", '.col1 h2')
+arrow1.drawFromTo(".callout1", '.col2 h2', {visible: false})
+arrow2.drawFrom(".callout1").drawTo('.col1 h2')
 
+// redrawn if the window changes size, responsive layouts
+window.onresize = function(){
+  arrow1.drawFromTo(".callout1", '.col2 h2')
+  arrow2.drawFromTo(".callout1", '.col1 h2')
+}
+
+// Example of animating in an arrow
+var animateCount = 0
+var timer = window.setInterval(function(){
+  arrow1.animateDraw(animateCount)
+
+  animateCount += 1
+  
+  if(animateCount > 100){
+    window.clearInterval(timer)
+  }
+}, 5)
+
+// Example of hooking up a slider
 var selector = document.querySelector('#x')
 selector.addEventListener("input", function() {
   arrow1.animateDraw(this.value)
